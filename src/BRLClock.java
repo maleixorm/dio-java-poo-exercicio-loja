1dcafbaa29273cd07c39bf938a28b1d6ea97263e
Marcos Aleixo <maknytz@gmail.com>
2026-03-30 18:30:18 -0300

    Implementando o método abstrato de converter horas

--- src/BRLClock.java
+++ src/BRLClock.java
@@ -1,3 +1,9 @@
 public class BRLClock extends Clock{
 
+    @Override
+    Clock convert(Clock clock) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convert'");
+    }
+
 }
