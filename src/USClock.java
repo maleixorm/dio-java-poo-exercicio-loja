1d8691e10af6bd0eef6622d4221c1f6e75caf307
Marcos Aleixo <maknytz@gmail.com>
2026-03-30 18:30:54 -0300

    Criando a classe USClock

---
+++ src/USClock.java
@@ -0,0 +1,40 @@
+public class USClock extends Clock {
+    private String periodIndicator;
+
+    public String getPeriodIndicator() {
+        return periodIndicator;
+    }
+
+    public void setPeriodIndicator(String periodIndicator) {
+        this.periodIndicator = periodIndicator;
+    }
+
+    public void setAfterMidday() {
+        this.periodIndicator = "PM";
+    }
+
+    public void setBeforeMidday() {
+        this.periodIndicator = "AM";
+    }
+
+    public void setHour(int hour) {
+        setBeforeMidday();
+        if ((hour > 12) && (hour <=23)) {
+            setAfterMidday();
+            this.hour = hour > 23 ? 0 : hour - 12;
+        }
+        else if (hour >= 24) {
+            this.hour = 0;
+        } 
+        else {
+            this.hour = hour;
+        }
+    }
+
+    @Override
+    Clock convert(Clock clock) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convert'");
+    }
+    
+}
