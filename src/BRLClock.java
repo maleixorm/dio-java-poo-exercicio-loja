bdf7683a8de8b8ad2406fc7ba32e89ef915981a3
Marcos Aleixo <maknytz@gmail.com>
2026-03-31 18:03:20 -0300

    Implementando o método abstrato de conversão de horas

--- src/BRLClock.java
+++ src/BRLClock.java
@@ -1,9 +1,14 @@
-public class BRLClock extends Clock{
+public non-sealed class BRLClock extends Clock{
 
     @Override
-    Clock convert(Clock clock) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convert'");
+    public Clock convert(Clock clock) {
+        this.second = clock.getSecond();
+        this.minute = clock.getMinute();
+        switch (clock){
+            case USClock usclock -> this.hour = (usclock.getPeriodIndicator().equals("PM")) ? usclock.getHour() + 12 : usclock.getHour();
+            case BRLClock brlClock -> this.hour = brlClock.getHour();
+        }
+        return this;
     }
 
 }
